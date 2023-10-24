482765996e1b69d00478cde463aec0a3aee92d56
James Dao <daothanhtu.edu@gmail.com>
2023-10-24 16:31:29 +0700

    turn back

--- Student_Exam.java
+++ Student_Exam.java
@@ -36,7 +36,7 @@ public class Student_Exam {
     }
 
     public void output() {
-        System.out.println("\nExam ID: changedddgitdd" + examID);
+        System.out.println("\nExam ID: " + examID);
     }
 
 }
