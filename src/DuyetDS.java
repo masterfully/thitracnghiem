7995900bad468c31cf8680c8e913300540ed78ea
masterfully <daothanhtu.edu@gmail.com>
2023-12-14 16:06:30 +0700

    delete files from vinhrepo

--- src/DuyetDS.java
+++
@@ -1,8 +0,0 @@
-public interface DuyetDS {
-    abstract public void timKiem();
-    abstract public void sapXep();
-    abstract public void themPT();
-    abstract public boolean xoaPT(int x);
-    abstract public void nhapDS();
-    abstract public void xuatDS();
-}
--- src/File.java
+++
@@ -1,4 +0,0 @@
-public interface File {
-    public void docFile();
-    public void ghiFile();
-}
